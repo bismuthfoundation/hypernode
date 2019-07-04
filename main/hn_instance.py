6f26b3940bce30b0629a345979ad854951b6a779
EggPool <dev@eggpool.net>
2019-07-04 14:41:10 +0200

    Add more debug files

--- main/hn_instance.py
+++ main/hn_instance.py
@@ -91,8 +91,10 @@ if __name__ == "__main__":
                 datadir = "./data"
                 config.load()
                 config.update_colored()
+                """
                 if len(config.POC_HYPER_NODES_LIST) <= 0:
                     config.POC_HYPER_NODES_LIST = config.POC_HYPER_NODES_LIST_0
+                """
                 ip = args.ip if args.ip else None
                 port = config.DEFAULT_PORT
                 suffix = ""
