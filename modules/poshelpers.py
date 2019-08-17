0beaaa02f8c2d1c2523fe5935ddcf11ea8f7a8e4
EggPool <dev@eggpool.net>
2019-08-17 09:48:44 +0200

    Format

--- modules/poshelpers.py
+++ modules/poshelpers.py
@@ -17,7 +17,7 @@ from logging import getLogger
 import config
 import poscrypto
 
-__version__ = '0.0.7'
+__version__ = "0.0.7"
 
 
 # GENERIC HELPERS ##############################################################
@@ -33,14 +33,16 @@ def download_file(url: str, filename: str):
     """
     app_log = getLogger("tornado.application")
     r = requests.get(url, stream=True)
-    total_size = int(r.headers.get('content-length')) / 1024
-    with open(filename, 'wb') as filename:
+    total_size = int(r.headers.get("content-length")) / 1024
+    with open(filename, "wb") as filename:
         chunkno = 0
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
                 chunkno = chunkno + 1
                 if chunkno % 10000 == 0:  # every x chunks
-                    app_log.info("Downloaded {} %".format(int(100 * (chunkno / total_size))))
+                    app_log.info(
+                        "Downloaded {} %".format(int(100 * (chunkno / total_size)))
+                    )
                 filename.write(chunk)
                 filename.flush()
         app_log.info("Downloaded 100 %")
@@ -63,7 +65,7 @@ def bootstrap(datadir):
     return True
 
 
-def update_source(url: str, app_log: logging.log=None):
+def update_source(url: str, app_log: logging.log = None):
     """
     Update source file from an url
 
@@ -88,14 +90,17 @@ def update_source(url: str, app_log: logging.log=None):
         """
     except Exception as e:
         if app_log:
-            app_log.warning("Something went wrong in update_source: {}, aborted".format(e))
+            app_log.warning(
+                "Something went wrong in update_source: {}, aborted".format(e)
+            )
         raise
+
 
 # DB #####################################################################
 
 
-def bool_to_dbstr(a_bool:bool):
-    return '1' if a_bool else '0'
+def bool_to_dbstr(a_bool: bool):
+    return "1" if a_bool else "0"
 
 
 # Height Helpers #####################################################################
@@ -115,7 +120,7 @@ def same_height(peer_status: dict, our_status: dict):
     return True
 
 
-def first_height_is_better(height_a: dict, height_b: dict):
+def first_height_is_better(height_a: dict, height_b: dict) -> bool:
     """
     Compares properties of the heights to tell which one is to keep in case of forks.
     Uses 'forgers', 'forgers_round', 'uniques', 'uniques_round', 'round', 'height'
@@ -140,13 +145,22 @@ def first_height_is_better(height_a: dict, height_b: dict):
         return True
     return False
     """
-    if height_a['block_hash'] == height_b['block_hash']:
+    if height_a["block_hash"] == height_b["block_hash"]:
         return False
-    sorted_heights = sorted([height_a, height_b],
-                          key=itemgetter('forgers', 'uniques', 'round', 'height', 'forgers_round',
-                                         'uniques_round', 'block_hash'),
-                          reverse=True)
-    return sorted_heights[0]['block_hash'] == height_a['block_hash']
+    sorted_heights = sorted(
+        [height_a, height_b],
+        key=itemgetter(
+            "forgers",
+            "uniques",
+            "round",
+            "height",
+            "forgers_round",
+            "uniques_round",
+            "block_hash",
+        ),
+        reverse=True,
+    )
+    return sorted_heights[0]["block_hash"] == height_a["block_hash"]
 
 
 def heights_match(height_a: dict, height_b: dict):
@@ -158,13 +172,21 @@ def heights_match(height_a: dict, height_b: dict):
     :param height_b: a height dict
     :return: boolean
     """
-    for key in ('height', 'round', 'sir', 'block_hash', 'uniques_round', 'forgers_round'):
+    for key in (
+        "height",
+        "round",
+        "sir",
+        "block_hash",
+        "uniques_round",
+        "forgers_round",
+    ):
         if height_a.get(key) != height_b.get(key):
             return False
     return True
 
 
 # Peer format helpers ###########################################################################
+
 
 def peer_to_fullpeer(peer: tuple):
     """
@@ -173,16 +195,17 @@ def peer_to_fullpeer(peer: tuple):
     :param peer:
     :return:
     """
-    return peer[1] + ':' + str(peer[2]).zfill(5)
+    return peer[1] + ":" + str(peer[2]).zfill(5)
 
 
 def ipport_to_fullpeer(ip: str, port: int):
-    return ip + ':' + str(port).zfill(5)
+    return ip + ":" + str(port).zfill(5)
 
 
 # Various helpers ###########################################################################
 
-def hello_string(port: int=101, posnet: str=None, address: str=None):
+
+def hello_string(port: int = 101, posnet: str = None, address: str = None):
     """
     Build hello string from params.
 
@@ -199,10 +222,10 @@ def hello_string(port: int=101, posnet: str=None, address: str=None):
     return posnet + str(port).zfill(5) + address
 
 
-def hello_to_params(hello:str, as_dict=False):
+def hello_to_params(hello: str, as_dict=False):
     posnet, port, peer_address = hello[:10], hello[10:15], hello[15:]
     if as_dict:
-        return {'posnet': posnet, 'port':int(port), 'address': peer_address}
+        return {"posnet": posnet, "port": int(port), "address": peer_address}
     return posnet, port, peer_address
 
 
@@ -217,7 +240,7 @@ def load_hn_temp():
     config.POC_HYPER_NODES_LIST = []
     try:
         i = 0
-        random.seed('my_fixed_seed')
+        random.seed("my_fixed_seed")
         weight_profile = [1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3]
         while os.path.isfile("hn_temp/mn{}.json".format(i)):
             with open("hn_temp/mn{}.json".format(i)) as f:
@@ -225,14 +248,21 @@ def load_hn_temp():
                 #  ('B8stX39s5NBFx746ZX5dcqzpuUGjQPJViC', '127.0.0.1', 6971, 1, "bis_addr_2", "bis_addr_2"),
                 # Weight distribution
                 weight = random.choice(weight_profile)
-                hn_tuple = (hn['address'], '127.0.0.1', 6969 + i, weight, "bis_addr_{}".format(i), "bis_addr_{}".format(i))
+                hn_tuple = (
+                    hn["address"],
+                    "127.0.0.1",
+                    6969 + i,
+                    weight,
+                    "bis_addr_{}".format(i),
+                    "bis_addr_{}".format(i),
+                )
                 config.POC_HYPER_NODES_LIST.append(hn_tuple)
             i += 1
         # print(config.POC_HYPER_NODES_LIST)
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print('detail {} {} {}'.format(exc_type, fname, exc_tb.tb_lineno))
+        print("detail {} {} {}".format(exc_type, fname, exc_tb.tb_lineno))
         sys.exit()
 
 
@@ -249,26 +279,46 @@ def fake_hn_dict(inactive_last_round, app_log):
     try:
         i = 0
         regs = {}
-        random.seed('my_fixed_seed')
+        random.seed("my_fixed_seed")
         weight_profile = [1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3]
         while os.path.isfile("hn_temp/mn{}.json".format(i)):
             with open("hn_temp/mn{}.json".format(i)) as f:
                 hn = json.load(f)
                 # Weight distribution
                 weight = random.choice(weight_profile)
-                regs["bis{}".format(i)] = dict(zip(['ip', 'port', 'pos', 'reward', 'weight', 'timestamp', 'active'],
-                                              ['127.0.0.1', 6969 + i, hn['address'], "bis{}".format(i), weight, 0, True]))
+                regs["bis{}".format(i)] = dict(
+                    zip(
+                        [
+                            "ip",
+                            "port",
+                            "pos",
+                            "reward",
+                            "weight",
+                            "timestamp",
+                            "active",
+                        ],
+                        [
+                            "127.0.0.1",
+                            6969 + i,
+                            hn["address"],
+                            "bis{}".format(i),
+                            weight,
+                            0,
+                            True,
+                        ],
+                    )
+                )
             i += 1
             if i > config.MAX_DEBUG_HN:
                 break
         for address, items in regs.items():
-            if items['pos'] in inactive_last_round:
-                regs[address]['active'] = False
+            if items["pos"] in inactive_last_round:
+                regs[address]["active"] = False
         return regs
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print('detail {} {} {}'.format(exc_type, fname, exc_tb.tb_lineno))
+        print("detail {} {} {}".format(exc_type, fname, exc_tb.tb_lineno))
         sys.exit()
 
 
